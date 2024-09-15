4a17cbbf04e8b48b4d651e92dd15a637d982aabc
lalekan <lekan.suleiman@gmail.com>
2024-09-15 14:46:32 -0700

    Everything added

--- server.js
+++ server.js
@@ -48,6 +48,29 @@ app.get("/fruits/:fruitId", async (req, res) => {
   res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+app.put('/fruits/:fruitId', async(req, res) => {
+  if(req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true
+  } else {
+    req.body.isReadyToEat = false
+  }
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body)
+  res.redirect("/fruits")
+})
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+
 
 
 
